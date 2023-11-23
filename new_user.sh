38e80aecc7d744ee19600f359fe3e4d03eb2ad85
alexxchen <alex_chen_mail@163.com>
2023-11-23 16:00:25 +0800

    docker_build_success

--- new_user.sh
+++
@@ -1,3 +0,0 @@
-curl 'http://localhost:3001/api/v1/user/regist' -H "Content-Type:application/json" -X POST -d '{"user_name": "yyy", "password": "wsy993327"}'
-#curl 'http://localhost:3001/api/v1/user/login' -H "Content-Type:application/json" -X POST -d '{"user_name": "admin", "password": "alexhope426"}'
-#curl 'http://localhost:3001/api/v1/role/list'
